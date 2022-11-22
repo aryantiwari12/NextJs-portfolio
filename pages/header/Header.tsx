11a6e6812bb83dc1d57623074058e6bed6fd3365
aryantiwari12 <aryantiwari1289@gmail.com>
2022-11-22 18:04:38 +0530

    all

--- pages/header/Header.tsx
+++ pages/header/Header.tsx
@@ -9,31 +9,37 @@ const Header = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-light ">
+            <nav className="navbar navbar-expand-lg ">
                 <div className="container-fluid">
-                    <a className="navbar-brand fs-1 font-weight-bold" href="#">AR<span className="text-danger">YAN</span> </a>
+                    <a className="navbar-brand fs-1 font-weight-bold hello" href="#">AR<span className="text-danger">YAN</span> </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse " id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
-                            <li className="nav-item font-weight-bold">
-                                <Link className="nav-link active" aria-current="page" href="#"><span className="text-dark font-weight-bold">HOME</span></Link>
+                            <li className="nav-item font-weight-bold ">
+                                <Link className="nav-link active" aria-current="page" href="#"><span className={`text-dark
+                                
+                                
+                                
+                                
+                                
+                                font-weight-bold hello`}>HOME</span></Link>
                             </li>
-                            <li className="nav-item font-weight-bold">
-                                <Link className="nav-link " href="#"><span className="text-dark font-weight-bold">ABOUT</span></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold">WORK</span></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold">SKILL</span></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold">CONTACT</span></Link>
+                            <li className="nav-item font-weight-bold ">
+                                <Link className="nav-link " href="#"><span className="text-dark font-weight-bold hello">ABOUT</span></Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold">RESUME</span></Link>
+                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold hello">WORK</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold hello">SKILL</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold hello">CONTACT</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#"><span className="text-dark font-weight-bold hello">RESUME</span></Link>
                             </li>
 
                             <label className="switch mt-2" >
